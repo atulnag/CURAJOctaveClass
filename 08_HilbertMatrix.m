8e686ccdb85559348ca99cf9902bdc533cde0eb1
Atul Nag <nag.atul@gmail.com>
2020-08-25 16:28:02 +0530

    Add files via upload

---
+++ 08_HilbertMatrix.m
@@ -0,0 +1,44 @@
+%% 
+%    Command statements
+%    Hilbert matrix using for-loops
+%
+%%
+
+% list dimensions
+m = 5;
+
+% initialize variables whenever possible!
+hilmat   = zeros(m);
+checkmat = 
+
+% now for the loops:
+for i=1:m
+    for j=1:m
+        
+         % denominator
+        den = i +j - 1
+        
+        % and invert to find this element
+        hilmat(i, j) = 1/den;
+        
+        
+        % bonus: checkerboard
+        
+        
+    end
+end
+
+% let's see what they look like
+figure(1), clf
+
+subplot(121)
+imagesc(hilmat)
+axis square, title('Hilbert matrix')
+set(gca,'xtick',[],'ytick',[])
+
+% subplot(122)
+% imagesc(checkmat)
+% axis square, title('Checkerboard matrix')
+% set(gca,'xtick',[],'ytick',[])
+
+%%
