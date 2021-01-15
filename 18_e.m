46dee393c8d8f49cd0bca4454cb126cb9b6c361b
Atul Nag <nag.atul@gmail.com>
2021-01-15 19:06:38 +0530

    Add files via upload

--- 18_e.m
+++ 18_e.m
@@ -1,68 +1,57 @@
 %%
-%    SECTION: Translate formulas into code
-%      VIDEO: Functions involving the natural exponent
+%    Complex numbers and Euler's formula
 %
 %%
 
-%% Laplace distribution
+%% complex numbers
+
+% components
+a =  2; % real part
+b = -3; % imaginary part
+
+% method 1: function 'complex'
+c1 = complex(a, b);
+
+% method 2: use imaginary operator
+c2 = a + i*b;
+
+% using i is a bad idea...
+c3 = a + 1i*b;
+ i = 3;
+c3 = a + i*b;
+
+% 1i is better because:
+1i = 3;
+
+%% Euler's formula (one of them...)
 
 % parameters
-lambda = 3; % lambda
-x = -5:.01:5;
+m = 3; % magnitude, or length of line
+p = 2*pi/3; % phase angle
 
-% here's the function
-f = 0.5 * lambda*exp(-lambda*abs(x));
+% Euler's formula and its cis equivalent
+e1 = m * exp( 1i*p);
+e2 = m * (cos(p) + 1i * sin(p) );
 
-plot(x,f);
-sum(f);
+% if e1==e2 then their difference is zero
+e1 - e2
 
-%normalize to probability density
-f = f./sum(f);  % scaling the function according to sum of all the values
+% recover key parameters (magnitude and phase)
+m1 = sqrt(real(e1)^2 + imag(el)^2);  % using Pythagorean theorem
+m2 = abs(e1);                        % using MATLAB function
 
-% plot the probability density
-figure(1), clf
-subplot(211)
-plot(x,f,'k','linew',3)
-xlabel('x'), ylabel('p( f(x) )')
-title('Probability density function')
+p1 = atan(imag(e1)/real(e1));         % using Pythagorean theorem
+p1 = angle(e1);% using MATLAB function
 
-% plot the cumulative density function
-subplot(212)
-plot(x,cumsum(f),'k','linew',3)
-xlabel('x'), ylabel('c( f(x) )')
-title('Cumulative density function')
+%% law of exponents
 
-%% log-normal distribution
+p = -4;
+q = 3;
 
-% x-range and function parameters
-% x = linspace(0,5,100);
-x = linspace(0.0001,5,100);
-m =  0;
-s = .5;
+e1 = exp(p+q)% e^{p+q}
+e2 = exp(p) * exp(q)% (e^p)(e^q)
 
-% PDF of the equation, in two parts
-part1 = 1./(x*s*sqrt(2*pi));  % denominator
-part2 = exp(-(log(x) - m).^2 / (2*s^2)); % numerator
 
-% combine the two parts
-lndist = part1 .* part2;
-
-plot(x,lndist);
-
-% convert to probability density
-lndist = lndist / sum(lndist); %normalizing
-
-% make a plot of the PDF
-figure(2), clf
-subplot(211)
-plot(x,lndist,'k','linew',2)
-xlabel('x'), ylabel('p( f(x) )')
-title('Probability density function')
-
-% plot the CDF
-subplot(212)
-plot(x,cumsum(lndist),'k','linew',2)
-xlabel('x'), ylabel('\Sigma p( f(x) )')
-title('Cumulative density function')
-
+% if e1==e2 then their difference is zero
+e1 - e2
 %%
